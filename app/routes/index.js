51efdb1c8d1ad4177bf62444d5129547e1132c7f
Ryan Carlile <ryancarlile@gmail.com>
2016-06-11 14:33:47 -0700

    Still fool with update and delete but moving on

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,18 +6,24 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
     save3(params) {
+      console.log("howdy");
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
+
     },
     update(question, params) {
-      debugger;
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
       question.save();
-      this.transitionTo('index');
-    },
-    destroyQuestion(question) {
-      question.destroyRecord();
       this.transitionTo('index');
     }
   }
