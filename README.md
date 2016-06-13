55ff3226fa9b9225efe862d5967cb28c2892a8a6
Ryan Carlile <ryancarlile@gmail.com>
2016-06-13 06:59:18 -0700

    Push what I have

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Q-and-a
+# Questions and Answers
 
-This README outlines the details of collaborating on this Ember application.
-A short introduction of this app could easily go here.
+An Ember JS application that manages a question and answer forum.
 
 ## Prerequisites
 
@@ -51,3 +50,8 @@ Specify what it takes to deploy your app.
   * [ember inspector for chrome](https://chrome.google.com/webstore/detail/ember-inspector/bmdblncegkenkacieihfhpjfppoconhi)
   * [ember inspector for firefox](https://addons.mozilla.org/en-US/firefox/addon/ember-inspector/)
 
+  ### License
+
+  *MIT License*
+
+  Copyright (c) 2016 **_Ryan Carlile_**
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,21 +4,8 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
-	actions: {
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    },
-		destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    }
-		saveAnswer3(params) {
+  actions: {
+    saveAnswer3(params) {
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
       question.get('answers').addObject(newAnswer);
@@ -26,6 +13,25 @@ export default Ember.Route.extend({
         return question.save();
       });
       this.transitionTo('question', params.question);
-    }
+    },
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    }  
   }
 });
