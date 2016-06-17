0445e8d5c2537ded4132e802a46149e4e00ad054
Ryan Carlile <ryancarlile@gmail.com>
2016-06-17 15:40:21 -0700

    ThumbUp toggle working

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	actions: {
+  thumbUpToggle: false,
+  thumbDownToggle: false,
+
+  actions: {
+		thumbUp: function() {
+			this.set('thumbUpToggle', true);
+
+    },
+    thumbDown: function() {
+      this.set('thumbDownToggle', true);
+    },
+
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
       }
     }
-	}
+  }
 });
+
+
+// css rotate(180)
